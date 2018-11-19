6a13420e2fc90e625e4b661c163fb272f94457dc
steven <steven_wu@sanwa-eng.com.tw>
2018-11-19 14:26:51 +0800

    upload test

--- TransferControl/Engine/RouteControl.cs
+++ TransferControl/Engine/RouteControl.cs
@@ -18,7 +18,7 @@ namespace TransferControl.Engine
 {
     public class RouteControl : AlarmMapping, Controller.ICommandReport, IDIOTriggerReport, IJobReport, ITaskJobReport
     {
-
+         //git upload test
         private static readonly ILog logger = LogManager.GetLogger(typeof(RouteControl));
         string _Mode = "";
         public bool IsInitial = false;
